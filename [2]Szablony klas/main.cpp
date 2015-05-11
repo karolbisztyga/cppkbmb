2156ff86d8b3d1b35c9e605eda162bc6bdaa4e3c
Travis Spears <travisspears0@gmail.com>
2015-05-11 22:18:17 +0200

    asd

--- [2]Szablony klas/main.cpp
+++ [2]Szablony klas/main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 
-  AghMatrix<int> aghM(3,2);
-  int tab[] = {1,5,8,2,6,2};
-  aghM.setItems(2,2,3,4,6,7,0,1);
+  AghMatrix<int> aghM(4,2);
+  int tab[] = {1,5,8,2,6,2,9,9};
+  aghM.setItems(2,2,3,4,6,7,0,9,1,2);
   aghM.print();
   cout << endl;
   aghM.setItems(tab);
